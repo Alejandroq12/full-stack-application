4625514682bde3c1f8b3003e8caed1a4c48a4c2c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-05 20:32:24 -0600

    Update: Header.js
    I added style to header.js in because I wanted to improve the nav bar.

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -17,9 +17,25 @@ const Logo = styled.h1`
   }
 `;
 
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
         <Logo>
           <Link href="/">Store</Link>
@@ -29,6 +45,6 @@ export default function Header() {
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
