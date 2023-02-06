71642fb127d964f64bdc017bdb3ad71e727641b7
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-06 13:33:06 -0600

    Fix: improving Product.ts
    I improved Product.ts in order to show price, options and a certain display mode.

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,4 +1,4 @@
-import { select, text } from '@keystone-next/fields/dist/fields.cjs';
+import { integer, select, text } from '@keystone-next/fields/dist/fields.cjs';
 import { list } from "@keystone-next/keystone/schema/dist/keystone.cjs";
 
 export const Product = list({
@@ -18,6 +18,12 @@ export const Product = list({
                 { label: 'Unavailable', value: 'UNAVAILABLE' },
             ],
             defaultValue: 'DRAFT',
-        })
+            ui: {
+                displayMode: 'segmented-control',
+                createView: { fieldMode: 'hidden' },
+            },
+        }),
+        price: integer(),
+        // TODO: Photo
     },
 });
