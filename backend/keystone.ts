61124572f949ac03dd9fe7435a79f5c56e042681
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-06 01:42:29 -0600

    Feature: setting up the backend
    I added the amount of time the user will be logged in. I added a configurations to avoid CORS-ORIGIN  problems, I created the connection to the database and I created some TODOS in order to remember later.

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,3 +1,33 @@
+import { config, createSchema } from '@keystone-next/keystone/schema/dist/keystone.cjs';
 import 'dotenv/config';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-store';
+
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360, // How long should they stay signed in?
+    secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+    // @ts-ignore
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true
+        }
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+        // Schema items go in here
+    }),
+    ui: {
+        // TODO: Change this for roles
+        isAccessAllowed: () => true,
+    },
+    //TODO: Add session values here
+});
