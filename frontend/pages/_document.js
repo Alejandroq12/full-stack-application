0dd0d7bbf245353972eefce87fcc6e26a6f08f83
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-06 00:21:35 -0600

    Update _document.js
    Adding server side rendering

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,16 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getinitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
