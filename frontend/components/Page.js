a5fdb1ca97756bc2a8ac8b7a04a08f0f45b878fc
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-05 19:18:50 -0600

    Update: including header inside the main page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children, cool }) {
   return (
     <div>
+      <Header />
       <h2>I am the page component</h2>
       <h3>{cool}</h3>
       {children}
