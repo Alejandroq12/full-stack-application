2ef6b3e55440c15622ee57218ff6b65f2a7e3e49
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-05 20:08:28 -0600

    Update: adding style to the logo
    I added style to the logo.

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,29 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2 rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem; 
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Here Logo</Link>
+        <Logo>
+          <Link href="/">Store</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
