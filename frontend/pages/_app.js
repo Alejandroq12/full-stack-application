774a351e33039ee99c8ece2ee05d7897acdaade3
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-07 00:31:45 -0600

    Feature: I added products the the main page
    I added style and the grid to show the products.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -11,7 +11,6 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps, apollo}) {
-  console.log(apollo);
   return (
     <ApolloProvider client={apollo}>
       <Page>
