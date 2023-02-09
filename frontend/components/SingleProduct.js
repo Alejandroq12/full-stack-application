f1621b3cf11718794b6d7ac194cd8f1735f41204
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-09 15:06:34 -0600

    Feature: show a single product
    Now when a single product is clicked the will render only than product

---
+++ frontend/components/SingleProduct.js
@@ -0,0 +1,43 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import DisplayError from './ErrorMessage';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!){
+    Product(where: { id: $id }) {
+      name
+      price
+      description
+      id
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleProduct({ id }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id,
+    },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+  const { Product } = data;
+  return (
+    <div>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </div>
+  );
+}
--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -1,19 +1,5 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
+import SingleProduct from '../../components/SingleProduct';
 
-const SINGLE_ITEM_QUERY = gql`
-query {
-    Product(where: { id: "63e16c3c66ab0646543f24b9" }) {
-      name
-      price
-      description
-    }
-  }
-`;
-
-export default function SingleProduct({ query }) {
-  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
-
-  console.log({data, loading, error});
-  return <p>Hey i am a single product { query.id }</p>;
+export default function SingleProductPage({ query }) {
+  return <SingleProduct id={ query.id } />;
 }
