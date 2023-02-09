c5d13951354be1e9811655c0fb933aefb8a43278
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-08 20:57:58 -0600

    Update CreateProduct.js
    I made possible to create a product and to show loading and errors if necessary.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -35,17 +36,24 @@ export default function CreateProduct() {
     price: 34234,
     description: 'These are the best shoes!',
   });
-  const payload = useMutation(CREATE_PRODUCT_MUTATION, {
-    variables: inputs,
-  });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
+        // submit the input fields to the backend:
+        const res = await createProduct();
+        console.log(res);
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input 
