2ae2ddd6d5b810eff3212f8706059c5de6c13bb6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-08 18:20:00 -0600

    Feature: Adding the fesature to sell a product
    I added the feature to sell a product by adding it with a picture, name, price, and description.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,34 +3,58 @@ import Form from './styles/Form';
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'Nice Shoes',
     price: 34234,
     description: 'These are the best shoes!',
   });
+
   return (
     <Form>
-      <label htmlFor="name">
-        Name
-        <input type="text"
-         id="name"
-         name="name"
-         placeholder="Name"
-         value={inputs.name}
-         onChange={handleChange}
-
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input type="number"
-         id="price"
-         name="price"
-         placeholder="price"
-         value={inputs.price}
-         onChange={handleChange}/>
-      </label>
-      <button type="button" onClick={clearForm}>Clear Form</button>
-      <button type="button" onClick={resetForm}>Reset Form</button>
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}               
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input 
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Add product</button>
+      </fieldset>
     </Form>
   );
 }
