b083140fb32a0843cf63aaddae8cb83e62653e2f
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-08 20:20:38 -0600

    Update CreateProduct.js
    I added a mutation to create a product with GraphQL.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,32 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in? and what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -7,6 +34,9 @@ export default function CreateProduct() {
     name: 'Nice Shoes',
     price: 34234,
     description: 'These are the best shoes!',
+  });
+  const payload = useMutation(CREATE_PRODUCT_MUTATION, {
+    variables: inputs,
   });
   return (
     <Form
