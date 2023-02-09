8208e195e04a77271e28e3c359e5e232773be9c1
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-09 14:30:13 -0600

    Update [id].js
    I made possible to query objects because I want them to render once we click on them.

--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -1,3 +1,19 @@
-export default function SingleProduct( { query }) {
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const SINGLE_ITEM_QUERY = gql`
+query {
+    Product(where: { id: "63e16c3c66ab0646543f24b9" }) {
+      name
+      price
+      description
+    }
+  }
+`;
+
+export default function SingleProduct({ query }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
+
+  console.log({data, loading, error});
   return <p>Hey i am a single product { query.id }</p>;
 }
