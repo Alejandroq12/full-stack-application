bf9270e2ce3206d3aeb9ba6c001521750a72eb76
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-08 21:39:49 -0600

    Feature: refetching
    Right now when a user creates a product the page re-renders automatically.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -41,6 +42,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
   return (
