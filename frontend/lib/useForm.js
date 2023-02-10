d04345ace2735011cd416afac61d124f2f2139df
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-10 16:30:32 -0600

    Feature: delete product
    I created a way to delete products.

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
-  const initialValues = Object.values(initial).join('')
+  const initialValues = Object.values(initial).join('');
 
   useEffect(() => {
     // This function runs when the things we are watching change
