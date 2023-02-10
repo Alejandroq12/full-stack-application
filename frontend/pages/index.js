92a6b46dda2117ce1f9bfb8ec649f071d50c1c66
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-09 19:58:54 -0600

    Update index.js
    I added important information in index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,18 @@
 export default function IndexPage() {
-  return (
-    <>
-      <p>Wellcome!</p>
-    </>
-  );
-}
+    return (
+      <>
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+          <h1 style={{ fontWeight: 'bold', fontSize: '3rem' }}>Bienvenidos a nuestra página web</h1>
+          <p style={{ fontSize: '1.2rem', color: 'gray' }}>En la cual encontrará una amplia variedad de productos de alta calidad.</p>
+        </div>
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+          <h2 style={{ fontWeight: 'bold', fontSize: '2.5rem' }}>Misión</h2>
+          <p style={{ fontSize: '1.5rem', color: 'gray' }}>Ofrecer a nuestros clientes productos de alta calidad a precios accesibles, brindando un servicio excepcional y fomentando una cultura de responsabilidad social y medioambiental.</p>
+        </div>
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+          <h2 style={{ fontWeight: 'bold', fontSize: '2.5rem' }}>Visión</h2>
+          <p style={{ fontSize: '1.5rem', color: 'gray' }}>Ser reconocidos como una de las tiendas líderes en la venta de productos de alta calidad, ofreciendo una experiencia única a nuestros clientes y contribuyendo al desarrollo sostenible de nuestra comunidad y del planeta.</p>
+        </div>
+      </>
+    );
+  }
