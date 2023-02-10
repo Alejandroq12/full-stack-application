d3227b701e10a54f02be32988a5a48e02cda181c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-10 16:50:04 -0600

    Update DeleteProduct.js
    I made possible to delete products from the backend and frontend because I was facing a problem do to the fact the a product must be erased from the backend but also from the frontend when remains as cache.

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -10,11 +10,18 @@ const DELETE_PRODUCT_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  console.log(payload);
+  console.log('running the update function after delete');
+  cache.evict(cache.identify(payload.data.deleteProduct))
+}
+
 export default function DeleteProduct({ id, children }) {
   const [deleteProduct, { loading, error }] = useMutation(
     DELETE_PRODUCT_MUTATION,
     {
       variables: { id },
+      update,
     }
   );
   return (
