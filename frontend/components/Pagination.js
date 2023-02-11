9aef3c175fb27bd8aec166854987d62e76ce9056
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-10 18:01:50 -0600

    Feature: count number of items on each page
    A number will be shown on each page to show the number of items.

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,12 +1,32 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import Head from 'next/head';
+import Link from 'next/link';
 import PaginationStyles from './styles/PaginationStyles';
+import DisplayError from './ErrorMessage';
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
 
 export default function Pagination({ page }) {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+  if (loading) return 'Loading...';
+  if (error) return <DisplayError error={error} />;
+  const { count } = data._allProductsMeta;
   return (
     <PaginationStyles>
       <Head>
         <title>La tiendita - Page {page} of___</title>
       </Head>
+      <Link href="/">⬅ Prev</Link>
+      <p>Page __ of __</p>
+      <p>{count} Items Total</p>
+      <Link href="/">Next ➡</Link>
     </PaginationStyles>
- );
+  );
 }
--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,9 +1,12 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 export default function ProductPage() {
-    return (
-      <div>
-        <Products />
-      </div>
-);
+  return (
+    <div>
+      <Pagination page={1} />
+      <Products />
+      <Pagination page={1} />
+    </div>
+  );
 }
