2ce1434dd6785e37335b2157111ed38abed6fbd4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-10 18:06:51 -0600

    Feature: number of pages
    I created a feature that allow us to see the number pages according to the number of items.

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import Head from 'next/head';
 import Link from 'next/link';
 import PaginationStyles from './styles/PaginationStyles';
+import { perPage } from '../config';
 import DisplayError from './ErrorMessage';
 
 const PAGINATION_QUERY = gql`
@@ -18,13 +19,14 @@ export default function Pagination({ page }) {
   if (loading) return 'Loading...';
   if (error) return <DisplayError error={error} />;
   const { count } = data._allProductsMeta;
+  const pageCount = count / perPage;
   return (
     <PaginationStyles>
       <Head>
         <title>La tiendita - Page {page} of___</title>
       </Head>
       <Link href="/">⬅ Prev</Link>
-      <p>Page __ of __</p>
+      <p>Page __ of {pageCount}</p>
       <p>{count} Items Total</p>
       <Link href="/">Next ➡</Link>
     </PaginationStyles>
