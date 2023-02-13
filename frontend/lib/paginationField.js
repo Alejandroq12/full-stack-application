5b03f2a9bca595c7f8403625cb8030532219bb3c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-02-12 22:37:00 -0600

    Update paginationField.js
    I added the feature to fetch items once we refresh the page or delete an item.

--- frontend/lib/paginationField.js
+++ frontend/lib/paginationField.js
@@ -15,17 +15,43 @@ export default function paginationField() {
 
       // Check if we have existing items.
       const items = existing.slice(skip, skip + first).filter((x) => x);
-      if (items.length !== first) {
-         // We don't have any items, we must go to the network to fetch them.
-         return false;
+      // If there are items and there are not enough items to sitisfy
+      // how many were requested and we are on the las page, then just send it.
+
+      if (items.length && items.length !== first && page === pages) {
+        return items;
       }
+
+      if (items.length !== first) {
+        // We don't have any items, we must go to the network to fetch them.
+        return false;
+      }
+      // If there are items, just return them from the cache, and we don't need to go to the network.
+      if (items.length) {
+        console.log(
+          `There are ${items.length} items in the cache! Gonna send them to Apollo!`
+        );
+        return items;
+      }
+
+      return false; // fallback to network.
+
       // First thing it does when it runs is asks the read function for those items.
       // We can either do one of two things:
       // First thing we can do is return the items because they are already in the cache.
       // The other thing we can do is to return false from here (network request).
     },
-    merge() {
-        // This runs when the Apollo client comes back from the network with our products.
+    merge(existing, incoming, { args }) {
+        const { skip, first } = args;
+      // This runs when the Apollo client comes back from the network with our products.
+      console.log(`Merging items from the network ${incoming.length}`);
+      const merged = existing ? existing.slice(0) : [];
+      for (let i = skip; i < skip + incoming.length; ++i) {
+        merged[i] = incoming[i - skip];
+      }
+      console.log(merged);
+      // Finally we return the merged items from the cache.
+      return merged;
     },
   };
 }
