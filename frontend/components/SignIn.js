d6673bdce563fa8d4fb479b2bba8a8cf1722868e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-16 19:23:35 -0600

    Add email input to Sign In component

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -2,8 +2,20 @@ import Form from './styles/Form';
 
 export default function SignIn() {
   return (
-    <Form>
-      <p>test</p>
+    <Form method="POST">
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            // value
+            // onChange
+          />
+        </label>
+      </fieldset>
     </Form>
   );
 }
