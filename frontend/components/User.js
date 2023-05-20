e8a6ac43c25c1d83fb9ef55ed642c5f041ce2a91
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-20 15:13:41 -0600

    Add sign out component

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,6 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
 
-export const CURRENT_USER_QUERY = gql`
+const CURRENT_USER_QUERY = gql`
   query {
     authenticatedItem {
       ... on User {
@@ -17,3 +17,5 @@ export function useUser() {
   const { data } = useQuery(CURRENT_USER_QUERY);
   return data?.authenticatedItem;
 }
+
+export { CURRENT_USER_QUERY };
